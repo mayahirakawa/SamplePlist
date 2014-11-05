2d2a5db51955c02ba05288f28a642f00f81e1807
Maya Hirakawa <candleshop.funny@gmail.com>
2014-11-05 16:40:13 +0800

    保存したよ☆（なんでコミットしたかを記入）

--- SamplePList/ViewController.h
+++ SamplePList/ViewController.h
@@ -8,8 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
 
+{
+    NSArray *_PListArray;
+
+}
+@property (weak, nonatomic) IBOutlet UITableView *PListtableview;
 
 @end
 
--- SamplePList/ViewController.m
+++ SamplePList/ViewController.m
@@ -16,8 +16,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+   
+    //オブジェクト内のファイルにアクセスできるオブジェクト宣言
+    NSBundle *bundle = [NSBundle mainBundle];
+    //読み込むプロパティリストのファイルパス（場所）を指定
+    NSString *path = [bundle pathForResource:@"friendPerArea" ofType:@"plist"];
+    
+    //プロパティリストの中身にデータを取得
+    NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
+    //取得できた配列のデータをメンバ変数に代入
+    _PListArray = [dic objectForKey:@"prefecuturelist"];
+    
+   // _PListArray = @[@"北海道",@"青森県",@"岩手県",];
+    
+    _PListtableview.delegate = self;
+    _PListtableview.dataSource =self;
+
+    
 }
+
+   // NSLog(@"配列の中身の数:%d",_PListArray.count);
+    
+    //_coffelisttableview.delegate = self;
+    
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+        return _PListArray.count;
+        
+}
+    
+-(UITableViewCell *)tableView:(UITableView *)tableView
+cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    //セルの名前
+    static NSString *cellIdentifier = @"cell";
+    //再利用可能なcellオブジェクトを作成
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:
+                             cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:
+                UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+}
+    
+    NSDictionary *FriendsWithArea = _PListArray[indexPath.row];
+
+    
+    
+    cell .textLabel.text = [FriendsWithArea objectForKey:@"Name"];
+    return cell;
+    
+
+
+
+
+}
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
